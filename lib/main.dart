d98a78de4bfcc91db496b18327debeb83fc9d564
AbdErrazak KENNICHE <91481473+abderrazak-hash@users.noreply.github.com>
2022-11-09 13:58:30 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ import 'package:flutter/material.dart';
 
 import 'package:emaie/constants.dart';
 import 'package:provider/provider.dart';
-import 'package:flutter_native_splash/flutter_native_splash.dart';
+//import 'package:flutter_native_splash/flutter_native_splash.dart';
 
 void main() {
   WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
